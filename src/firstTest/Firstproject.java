432aaa19a86c9bd1c41be8364001ed738957cce8
durga <durganayak55@gmail.com>
2022-09-13 22:47:41 +0530

    resolved the conflict

--- src/firstTest/Firstproject.java
+++ src/firstTest/Firstproject.java
@@ -7,19 +7,9 @@ public class Firstproject {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
-		System.out.println();
-		System.out.println();
-=======
-		int a= 10; int d;
-		int b=20;
-		int c;
-		d =b/a;
-		c= a+b; 
-		System.out.println(c);
-		System.out.println(d);
 
->>>>>>> 42f9bdc0a87725acd5647a67c060b33f02bcc6c0
+		System.out.println();
+		
 	}
 
 }
