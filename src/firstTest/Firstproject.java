d4c1b8f81f2e854cd047db4d692cd8d8e4cd08f9
durga <durganayak55@gmail.com>
2022-09-15 17:08:54 +0530

    summation

--- src/firstTest/Firstproject.java
+++ src/firstTest/Firstproject.java
@@ -7,8 +7,10 @@ public class Firstproject {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int a=10; int b=20; int c;
+		c =a+b;
 
-		System.out.println();
+		System.out.println(c);
 		
 	}
 
