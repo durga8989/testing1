b15acf9d25177dc06d4bb2899db4316185346283
durga <durganayak55@gmail.com>
2022-09-15 17:15:31 +0530

    division

--- src/firstTest/Firstproject.java
+++ src/firstTest/Firstproject.java
@@ -7,10 +7,12 @@ public class Firstproject {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a=10; int b=20; int c;
+		int a=10; int b=20; int c;int d;
 		c =a+b;
+		d=b/a;
 
 		System.out.println(c);
+		System.out.println(d);
 		
 	}
 
